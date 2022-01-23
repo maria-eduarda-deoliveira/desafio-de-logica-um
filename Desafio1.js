70a1eb06dd1e33b0f893e1ae16f66b7f9dd76d71
Maria Eduarda <90702728+maria-eduarda-deoliveira@users.noreply.github.com>
2022-01-23 15:23:25 -0300

    Add files via upload

---
+++ Desafio1.js
@@ -0,0 +1,12 @@
+const nome = "Primeiro Desafio";
+var number = 15;
+var year = 2022;
+var number = year + number;
+console.log(2037);
+
+console.log('Estou mexendo no console.log');
+
+var quartafeira = true;
+if (quartafeira) {
+  console.log('true');
+}
